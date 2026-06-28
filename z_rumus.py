b6972de24e02cd9f7e3d22becb7c2b35fee95d85
Faris Fatur Rohman <166606730+SubaerVernandes@users.noreply.github.com>
2026-06-29 04:05:22 +0700

    Delete z_rumus.py

--- z_rumus.py
+++
@@ -1,6 +0,0 @@
-# rumus yang di taruh di file ini "rumus.py" akan di gunakan pada file "implementasi_rumus.py"
-
-def add(x, y):
-    return (x + y)
-
-employee = {"Name":"John", "Age":24, "Gender":"Male"}
