beba580af1f0142d09926799f002062fd4a1d152
Faris Fatur Rohman <166606730+SubaerVernandes@users.noreply.github.com>
2026-06-29 04:04:41 +0700

    Add files via upload

---
+++ z_rumus.py
@@ -0,0 +1,6 @@
+# rumus yang di taruh di file ini "rumus.py" akan di gunakan pada file "implementasi_rumus.py"
+
+def add(x, y):
+    return (x + y)
+
+employee = {"Name":"John", "Age":24, "Gender":"Male"}
